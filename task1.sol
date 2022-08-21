ec794d0a5ecc6127c38db0bea6c608c12deaccd8
BishamKhan <80125979+BishamKhan@users.noreply.github.com>
2022-08-21 11:13:47 +0500

    Create task1.sol

---
+++ task1.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.4;
+/**
+ * @title Twitter Contract
+ * @dev Store & retrieve value in a variable
+ */
+
+contract hw1{
+
+    string public str = "Hello Solidity";
+
+
+}
